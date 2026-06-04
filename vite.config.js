00fca4b7a95bb205125217a53facdd38cfcaffe9
xxvii <65766919+DeathThe27th@users.noreply.github.com>
2026-06-04 08:38:19 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   optimizeDeps: {
     include: [
       '@mysten/dapp-kit',
       '@mysten/sui',
+      '@mysten/sui/transactions',
+      '@mysten/sui/client',
       '@tanstack/react-query',
     ],
+    force: true,
   },
   resolve: {
-    dedupe: ['@mysten/sui'],
+    dedupe: ['@mysten/sui', '@mysten/dapp-kit'],
   },
 })
